a4281bc105191bdd9f36aeed38584861fcf47980
Li Li <leelogantom@gmail.com>
2018-01-21 23:43:32 -0500

    Redditbot result
    Result of a Redditbot

--- pa_chong/scrapy_tutorial/tutorial/tutorial/spiders/redditbot.py
+++ pa_chong/scrapy_tutorial/tutorial/tutorial/spiders/redditbot.py
@@ -8,4 +8,19 @@ class RedditbotSpider(scrapy.Spider):
     start_urls = ['http://www.reddit.com/r/gameofthrones//']
 
     def parse(self, response):
-        pass
+        # Extract the content using css selectors
+        titles = response.css('.title.may-blank::text').extract()
+        votes = response.css('.score.unvoted::text').extract()
+        times = response.css('time::attr(title)').extract()
+        comments = response.css('.comments::text').extract()
+
+        for item in zip(titles, votes, times, comments):
+            scraped_info = {
+                'title': item[0],
+                'vote': item[1],
+                'created at': item[2],
+                'comments': item[3],
+            }
+
+            yield scraped_info
+
